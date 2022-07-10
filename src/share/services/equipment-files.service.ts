3243843cd608127cdd2294edd85ac94badbeb30b
Sergey <s_kulaev@bk.ru>
2022-07-10 19:41:48 +0700

    fix run script and prittier

--- src/share/services/equipment-files.service.ts
+++ src/share/services/equipment-files.service.ts
@@ -4,17 +4,16 @@ import {
   HttpStatus,
   Injectable,
 } from '@nestjs/common';
-import * as util from 'util';
 import * as ChildProcess from 'child_process';
 import * as fs from 'fs';
-import * as path from 'path';
+import * as util from 'util';
 import { Output } from '../response/output.interface';
 
 @Injectable()
 export class EquipmentFilesService {
   async findSourceFileInWindows(
     fileExtension: string,
-    directoryPath: string = 'C:\\Scripts\\',
+    directoryPath = 'C:\\Scripts\\',
   ) {
     const exec = util.promisify(ChildProcess.exec);
     const { stdout: files } = await exec(
@@ -32,22 +31,26 @@ export class EquipmentFilesService {
   async runScript(command: string): Promise<Output> {
     const exec = util.promisify(ChildProcess.exec);
     const { stdout, stderr } = await exec(command);
-    console.log('--------------');
-    console.log('command: ', command);
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
-    console.log('--------------');
+    this.logResultScript(command, stdout, stderr);
     if (stderr)
-      new HttpException(
+      throw new HttpException(
         `Some error during execution or start up a script \n stderr: ${stderr}`,
         HttpStatus.SERVICE_UNAVAILABLE,
       );
     return await this.getLogs();
   }
 
+  private logResultScript(command: string, stdout: string, stderr: string) {
+    console.log('--------------');
+    console.log('command: ', command);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+    console.log('--------------');
+  }
+
   async getLogs(
-    directoryPath: string = 'C:\\Scripts\\',
-    fileName: string = 'Log.txt',
+    directoryPath = 'C:\\Scripts\\',
+    fileName = 'Log.txt',
   ): Promise<Output> {
     const readFile = util.promisify(fs.readFile);
     const logs: string = await readFile(directoryPath + fileName, 'utf8');
@@ -56,7 +59,7 @@ export class EquipmentFilesService {
 
   async saveFile(
     file: Express.Multer.File,
-    directoryPath: string = 'C:\\Scripts\\',
+    directoryPath = 'C:\\Scripts\\',
   ): Promise<string> {
     try {
       await fs.promises.writeFile(

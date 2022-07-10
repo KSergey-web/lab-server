e7d372ce4d38fe38e5c0854e968b7e5d7338583a
Sergey <s_kulaev@bk.ru>
2022-07-10 19:52:35 +0700

    deleted tests

--- src/altera-de1-so-c/altera-de1-so-c.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AlteraDe1SoCInterceptor } from './altera-de1-so-c.interceptor';
-
-describe('AlteraDe1SoCInterceptor', () => {
-  it('should be defined', () => {
-    expect(new AlteraDe1SoCInterceptor()).toBeDefined();
-  });
-});
--- src/stk500/stk500.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Stk500Interceptor } from './stk500.interceptor';
-
-describe('Stk500Interceptor', () => {
-  it('should be defined', () => {
-    expect(new Stk500Interceptor()).toBeDefined();
-  });
-});
